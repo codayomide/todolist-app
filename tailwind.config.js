42b6c15eede595b5a30f9e8518ba7ebb24931514
codayomide <smokayomide@gmail.com>
2025-03-15 20:49:19 +0100

    Fixed issue with images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "primaryBlue": "hsl(220, 98%, 61%)",
-        "lmLightGray": "#FAFAFA", 
-        "lmVeryLightGrayBlue": "#EDEDF5",
-        "lmLightGrayBlue": "#D2D3E0",
-        "lmDarkGrayBlue": "#9799A8", 
-        "lmVeryDarkGrayBlue": "#5D6476", 
+        primaryBlue: "hsl(220, 98%, 61%)",
+        lmLightGray: "#FAFAFA",
+        lmVeryLightGrayBlue: "#EDEDF5",
+        lmLightGrayBlue: "#D2D3E0",
+        lmDarkGrayBlue: "#9799A8",
+        lmVeryDarkGrayBlue: "#5D6476",
 
-        "dmVeryBlue": "#1C2028", 
-        "dmDarkDesBlue": "#252D37", 
-        "dmLightGrayBlue": "#D1D5DB", 
-        "dmLightGrayBlueHover": "#EDEDF5",
-        "dmDarkGrayBlue": "#848A9D", 
-        "dmVeryDarkGrayBlue": "#3A4158", 
+        dmVeryBlue: "#1C2028",
+        dmDarkDesBlue: "#252D37",
+        dmLightGrayBlue: "#D1D5DB",
+        dmLightGrayBlueHover: "#EDEDF5",
+        dmDarkGrayBlue: "#848A9D",
+        dmVeryDarkGrayBlue: "#3A4158",
       },
       backgroundImage: {
-        "imgBgMobileLight": "url('./src/assets/images/bg-mobile-light.jpg')",
-        "imgBgMobileDark": "url('./src/assets/images/bg-mobile-dark.jpg')",
-        "imgBgDesktopLight": "url('./src/assets/images/bg-desktop-light.jpg')",
-        "imgBgDesktopDark": "url('./src/assets/images/bg-desktop-dark.jpg')",
-        "checkGradientBg": "linear-gradient(90deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
-      }
+        imgBgMobileLight: "url('/images/bg-mobile-light.jpg')",
+        imgBgMobileDark: "url('/images/bg-mobile-dark.jpg')",
+        imgBgDesktopLight: "url('/images/bg-desktop-light.jpg')",
+        imgBgDesktopDark: "url('/images/bg-desktop-dark.jpg')",
+        checkGradientBg:
+          "linear-gradient(90deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+      },
     },
   },
   plugins: [],
-}
+};
