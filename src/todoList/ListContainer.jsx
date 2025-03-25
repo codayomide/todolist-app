9bee0b644cd3e7f30b5aaa1dc4ab7222c68b28e0
codayomide <smokayomide@gmail.com>
2025-03-25 12:48:00 +0100

    Removed unused comment

--- src/todoList/ListContainer.jsx
+++ src/todoList/ListContainer.jsx
@@ -15,21 +15,6 @@ const ListContainer = ({
   const { theme } = useContext(ThemeContext);
 
   const handleReorder = (newOrder) => {
-    // setItems((prevList) => {
-    //   let updatedList = [...prevList];
-
-    //   const reorderedIDs = newOrder.map((item) => item.id);
-
-    //   let index = 0;
-    //   updatedList = updatedList.map((item) => {
-    //     if (reorderedIDs.includes(item.id)) {
-    //       return newOrder[index++];
-    //     }
-    //     return item;
-    //   });
-
-    //   return updatedList;
-    // });
     console.log("New Order Received:", newOrder);
     setItems(newOrder);
 
